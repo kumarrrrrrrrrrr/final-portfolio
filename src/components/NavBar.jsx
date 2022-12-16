2127a590a726ddf708000180689efb1b5a4639eb
kumarrrrrrrrrrr <bharathvel606084@gmail.com>
2022-12-16 08:49:09 +0530

    init

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Yash</h1>
+        <h1 className="text-5xl font-signature ml-2">Bharath</h1>
       </div>
 
       <ul className="hidden md:flex">
